0dd7196204965270060f0c5a0ae8a073c89c9011
Xavierj9o <xavieflash@live.com>
2024-08-07 20:39:06 -0500

    commit 3, average fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,10 @@
 function getAverage(scores) {
     let sum = 0;
-    for (let note in scores){
-      sum += note
-      console.log(note)
+    for (let i = 0; i < scores.length; i++){
+      sum += scores[i];
     }
-    console.log(sum);
-    sum = sum / scores.length;
-    console.log(sum);
-    return sum;
+    let average = sum / scores.length;
+    return average;
   }
   
   console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
