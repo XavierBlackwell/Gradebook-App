3a125791075a33895731c7ebcd72b9ea8f941c8f
Xavierj9o <xavieflash@live.com>
2024-08-07 20:36:18 -0500

    commit 2, add up scores and divide for average

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
 function getAverage(scores) {
-
-}
-
-console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+    let sum = 0;
+    for (let note in scores){
+      sum += note
+      console.log(note)
+    }
+    console.log(sum);
+    sum = sum / scores.length;
+    console.log(sum);
+    return sum;
+  }
+  
+  console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+  console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
